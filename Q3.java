04a0bd4d9c840a57867665f9627af223859736c9
Vivek singh <iamvivekrajawat@gmail.com>
2025-02-01 06:25:13 +0530

    next

--- Q3.java
+++ Q3.java
@@ -1,12 +1,7 @@
 public class Q3 {
     public static void main(String[] args) {
         System.out.println("Hii vivek");
-        System.out.println("sdkjsdjk");
     }
-    {
-        System.out.println("How to dlt");
-        System.out.println("how to insert");
-    }
+   
 
 }
-
