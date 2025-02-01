7dd8baf88e20821318e9c8114550a7442187c249
Vivek singh <iamvivekrajawat@gmail.com>
2025-02-01 06:14:42 +0530

    commit

--- Q2.java
+++ Q2.java
@@ -2,5 +2,9 @@ public class Q2 {
     public static void main(String[] args) {
         System.out.println("hiii");
     }
+    {
+        System.out.println("dsfk");
+        System.out.println("dkfj");
+    }
     
 }
