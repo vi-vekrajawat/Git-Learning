5abd54e8a9e63c74ca3d9073ceb6f08f79e09a32
Vivek singh <iamvivekrajawat@gmail.com>
2025-02-03 07:02:05 +0530

    master changing

--- Q2.java
+++ Q2.java
@@ -1,6 +1,8 @@
 public class Q2 {
     public static void main(String[] args) {
         System.out.println("hiii");
+        int a =48;
+        int b = 45;
     }
     {
         System.out.println("dsfk");
