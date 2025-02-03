ab68efc32438aad9cb57bd75e2940aaafbcf93da
Vivek singh <iamvivekrajawat@gmail.com>
2025-02-03 22:53:29 +0530

    Second commit

--- Que1.java
+++ Que1.java
@@ -2,6 +2,9 @@ public class Que1 {
     public static void main(String[] args) {
         int x = 10;
         int y = 20;
+        int a = 20;
+        int b = 30;
+        System.out.println(a+b);
         System.out.println("Integer "+x);
         System.out.println("Integer1 "+y);
         System.out.println("Clear");
