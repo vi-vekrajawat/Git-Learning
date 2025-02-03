226ea061658cc01de7d9448d0b650902fd6d5c4c
Vivek singh <iamvivekrajawat@gmail.com>
2025-02-03 06:09:42 +0530

    NewBranch first commit

---
+++ Second.java
@@ -0,0 +1,10 @@
+public class Second {
+    public static void main(String[] args) {
+        System.out.println("Second file");
+        System.out.println("Good morining");
+        System.out.println("Hii vivek");
+        System.out.println("Update balance");
+        System.out.println("functio");
+    }
+    
+}
---
+++ first.java
@@ -0,0 +1,9 @@
+public class first {
+    public static void main(String[] args) {
+        System.out.println("another file");
+        System.out.println("jai shri ram");
+        System.out.println("jai bajrangi");
+    }
+
+    
+}
