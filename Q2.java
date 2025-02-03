48b9dd1c50b598efcad7a28429a402443e3ae43b
Vivek singh <iamvivekrajawat@gmail.com>
2025-02-03 22:50:53 +0530

    First commit

--- Q2.java
+++ Q2.java
@@ -1,15 +1,21 @@
 import java.math.*;
 public class Q2 {
     public static void main(String[] args) {
-        System.out.println("hiii");
+        // System.out.println("hiii");
         int a =48;
         int b = 45;
+
+        int min=100;
+        int max=200;
+        int otp=(int)(Math.random()*(max-min+1)+min);
+        System.out.println(otp);
     }
     {
-        System.out.println("dsfk");
-        System.out.println("dkfj");
-        System.out.println("sdk");
-        System.out.println("first");
+        // System.out.println("dsfk");
+        // System.out.println("dkfj");
+        // System.out.println("sdk");
+        // System.out.println("first");
+
     
 }
 }
---
+++ Que1.java
@@ -0,0 +1,10 @@
+public class Que1 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        System.out.println("Integer "+x);
+        System.out.println("Integer1 "+y);
+        System.out.println("Clear");
+    }
+    
+}
