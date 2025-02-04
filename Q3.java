318862de1dacda3da0355f2c7b18970a442d55b0
Vivek singh <iamvivekrajawat@gmail.com>
2025-02-04 06:13:31 +0530

    first

--- Q3.java
+++ Q3.java
@@ -1,6 +1,7 @@
 public class Q3 {
     public static void main(String[] args) {
-        System.out.println("Hii vivek");
+        System.out.println("Good maoring ");
+        System.out.println("Jai shri ram");
     }
    
 
