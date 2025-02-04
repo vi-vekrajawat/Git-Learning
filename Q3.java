2ff5916e48469c16937640e102ccf586f0408a52
Vivek singh <iamvivekrajawat@gmail.com>
2025-02-04 06:35:53 +0530

    commit1

--- Q3.java
+++ Q3.java
@@ -10,6 +10,9 @@ public class Q3 {
         for(int i=0;i<arr.length;i++){
             System.out.println(arr[i]);
         }
+        int x =5;
+        int y =7;
+        System.out.println(x*y);
     }
    
 
