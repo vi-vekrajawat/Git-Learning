aa327079bc1530d26a748999614ff47240b0570e
Vivek singh <iamvivekrajawat@gmail.com>
2025-02-04 22:15:53 +0530

    first commit

---
+++ GitHub/First.java
@@ -0,0 +1,8 @@
+class First{
+    public static void main(String[] args) {
+        System.out.println("jai shri ram");
+        System.out.println("Jai bajrangbali");
+        System.out.println("Radhe - Radhe");
+        System.out.println("Om Namah Shivay");
+    }
+}
