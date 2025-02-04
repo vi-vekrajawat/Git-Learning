6ae7655ee881653ca822d5c1303be77a38a0a9d2
Vivek singh <iamvivekrajawat@gmail.com>
2025-02-04 22:44:10 +0530

    First1

---
+++ GitHub/First1.java
@@ -0,0 +1,8 @@
+public class First1 {
+    public static void main(String[] args) {
+        System.out.println("branch ");
+        System.out.println("Load on git hub");
+        System.out.println("latest");
+    }
+    
+}
