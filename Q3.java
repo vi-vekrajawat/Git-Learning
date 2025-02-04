72daba29d45d89bfdaf5d920f1b61708983fd0a0
Vivek singh <iamvivekrajawat@gmail.com>
2025-02-04 06:14:46 +0530

    second commit

--- Q3.java
+++ Q3.java
@@ -2,6 +2,9 @@ public class Q3 {
     public static void main(String[] args) {
         System.out.println("Good maoring ");
         System.out.println("Jai shri ram");
+
+        System.out.println("good evening");
+        System.out.println("jai bajrag bali");
     }
    
 
